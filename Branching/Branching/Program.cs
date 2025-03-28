7de8fd67978178f2d4520c144e218755ee0e8900
KaityBadge <kaitybadge@yahoo.com>
2025-03-27 20:23:27 -0700

    added comments to code

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomTempurature = 70;
+
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTempurature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (roomTempurature > currentTemperature)
+            {
+                Console.WriteLine("It is colder than room temperature.");
+            }
+            else if (currentTemperature > roomTempurature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. ");
+            }
+
+            Console.ReadLine();
+
+
+            //int currentTemp = 70;
+            //int roomTemp = 70;
+
+            ////This '?' is the ternary operator
+            ////it checks the condition of the statement and prints the string on the left side of the colon if true
+            ////if the condition is false, the string on the right of the colon will print to console.
+            //string comparisonResult = currentTemp == roomTemp ? "it is room temp" : "it is not room temp";
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+
+
+            ////first do this, if it it is true, the code in the curly braces will print in console
+            ////if this evaluates the rest of the code can be skipped
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            ////If the first statement isn't true then check this
+            ////If this is true then print what's in the curly braces
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temp.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("Room temp is warmer than current temp.");
+            //}
+            ////If neither of the above statements are true do this else statement
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temp");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ PackageShippingQuote/PackageShippingQuote/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Branching
+{
+    class PackageShippingQuote
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("\nPlease enter the weight of your package: ");
+            //converts user input to decimal and stores it in packageWeight variable
+            decimal packageWeight = Convert.ToDecimal(Console.ReadLine());
+            //if the package weight is more than 50 then print the string in the console.
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            //else if the package weight is less than 50 then print the following string
+            else if (packageWeight < 50)
+            {
+                Console.WriteLine("\nPlease enter the width of your package: ");
+            }
+
+            //converts user input to decimal and stores it in package width variable
+            decimal packageWidth = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the height of your package: ");
+            //converts user input to decimal and stores it in package height variable 
+            decimal packageHeight = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the length of your package: ");
+            //converts user input to decimal and stores it in package length variable
+            decimal packageLength = Convert.ToDecimal(Console.ReadLine());
+            //if the width + length + height is more than 50, the string in the curly braces will print in the console
+            if (packageWidth + packageLength + packageHeight > 50)
+            {
+                Console.WriteLine("\nPackage too big to be shipped via package express.");
+            }
+            //creates a decimal variable called shipping cost
+            //multiplies the height, length and width then multiplies that total by the weight and divide the outcome by 100
+            decimal shippingCost = ((packageHeight * packageLength * packageWidth) * packageWeight) / 100;
+            //concatenates the string + the value of the shippingCost variable
+            Console.WriteLine("\nYour estimated total for shipping this package is: $" + shippingCost);
+            Console.ReadLine();
+
+        }
+    }
+}
