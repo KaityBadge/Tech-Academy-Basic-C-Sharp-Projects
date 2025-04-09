36d7be504c1870120e24625f05f6e23e2e42e5fb
KaityBadge <kaitybadge@yahoo.com>
2025-04-09 11:56:13 -0700

    method overload practice

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathOperation
+    {
+        // This method takes two INTEGERS as parameters and returns their sum.
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        //Another method for the MathOperation class that is also called Add.
+        //This method takes two DECIMALS as parameters and returns their sum.
+        public decimal Add(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+
+        //Another method for the MathOperation class that is also called Add.
+        //This method takes the STRING parameters, converts them to INTEGERS, and returns the sum of those integers
+        public string Add(string number1, string number2)
+        {
+            // Convert the strings (number1 and number2) to integers and stores them as integers called num1 and num2
+            int num1 = Convert.ToInt32(number1);
+            int num2 = Convert.ToInt32(number2);
+            // Return the sum of the integers as a string
+            return (num1 + num2).ToString();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates the MathOperation class
+            MathOperation math = new MathOperation();
+
+            //calls the Add method with two INTEGERS passed in as parameters
+            int intSum = math.Add(17, 106);
+            Console.WriteLine("The sum of 17 and 106 is: " + intSum);
+
+            //calls the Add method with two DECIMALS passed in as parameters
+            decimal decimalSum = math.Add(16.1m, 2.3m);
+            Console.WriteLine("The sum of 16.1 and 2.3 is: " + decimalSum);
+
+
+            //calls the Add method with two STRINGS passed in as parameters
+            string stringSum = math.Add("2", "100");
+            Console.WriteLine("The sum of 2 and 100 is: " + stringSum);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
