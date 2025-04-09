c42d8890f5c729852e98572502b64ab88631b75e
KaityBadge <kaitybadge@yahoo.com>
2025-04-09 09:27:44 -0700

    calls methods outside of class

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+
+    /// This class contains methods for addition, subtraction, and multiplication.
+    /// This class is public so it can be accessed outside of this class.
+    public class Math
+    {
+        // public method to add two integers
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        //public method to subtract two integers
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        //public method to multiply two integers
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do math operations on?");
+            //convert the user input to an integer
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //creates/initiates an object called math of the class Math
+            Math math = new Math();
+
+
+            Console.WriteLine("The number you entered is: " + userInput);
+            Console.WriteLine(userInput + " plus 10 equals: ");
+
+            //calls the Add method from the Math class and passes in the userInput and 10
+            Console.WriteLine(math.Add(userInput,10));
+
+
+            Console.WriteLine(userInput + " minus 5 equals: ");
+            //calls the Subtract method from the Math class and passes in the userInput and 5
+            Console.WriteLine(math.Subtract(userInput, 5));
+
+
+            Console.WriteLine(userInput + " multiplied by 2 equals: ");
+            //calls the Multiply method from the Math class and passes in the userInput and 2
+            Console.WriteLine(math.Multiply(userInput, 2));
+
+            Console.ReadLine();
+        }
+    }
+}
