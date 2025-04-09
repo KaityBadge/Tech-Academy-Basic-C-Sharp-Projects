06795d1fd77c2a0f00da29ce3f50657d812fc0a8
KaityBadge <kaitybadge@yahoo.com>
2025-04-09 11:00:27 -0700

    use of method overloading

--- TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
+++ TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720392c5a5f0f8d785266ec6613bf8f7f9be74b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c42d8890f5c729852e98572502b64ab88631b75e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
