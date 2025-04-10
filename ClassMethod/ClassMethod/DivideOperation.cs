8403e69fb3e6aa2e1c7c419fcd8aa75109ecc19e
KaityBadge <kaitybadge@yahoo.com>
2025-04-10 11:44:27 -0700

    create a void method in DivideByTwo class

---
+++ ClassMethod/ClassMethod/DivideOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    public class DivideOperation()
+    {
+        //creates a void method that takes integer called userNumber and divides it by 2
+        public void DivideByTwo(int userNumber)
+        {
+            //divides the userNumber by 2
+            int result = userNumber / 2;
+
+            //displays the result to the user
+            Console.WriteLine($"The result of dividing {userNumber} by 2 is: {result}");
+        }
+
+        //creates an overload method of class DivideByTwo with output parameters
+        //out int is used to return the result of the division
+        public static void DivideByTwo(int userNumber, out int result)
+        {
+            //divides the userNumber by 2
+            result = userNumber / 2;
+            //displays the result to the user
+            Console.WriteLine($"The result of dividing {userNumber} by 2 is: {result}");
+        }
+
+    }
+
+}
---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates the DivideOperation class
+            DivideOperation divide = new DivideOperation();
+
+            //prompts the user to enter a number
+            Console.WriteLine("Please enter a number to divide by 2:");
+
+            //creates an integer variable to store the user input
+            int userNumber;
+
+            //calls the DivideByTwo method
+            //if the input is an integer then perform the divide method
+            //tryparse will check if the input is an integer
+            //the out parameter will store the user input as an integer
+            if (int.TryParse(Console.ReadLine(), out userNumber))
+            {
+                divide.DivideByTwo(userNumber);
+            }
+            //if the input is not an integer then display an error message
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
