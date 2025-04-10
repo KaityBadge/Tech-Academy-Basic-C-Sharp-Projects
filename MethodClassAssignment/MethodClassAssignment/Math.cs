9a3d7f838d7dcdb0d10f997619838beeddef71a7
KaityBadge <kaitybadge@yahoo.com>
2025-04-10 10:18:16 -0700

    void method and specifies parameters by name

---
+++ MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Math
+    {
+        // Creates a void method called Add that takes two integers as parameters
+        public void Add(int a, int b)
+        {
+            //math operation on the first integer
+            int addition = a + b;
+
+            //displays the second integer on the console
+            Console.WriteLine("The second integer is: " + b);
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates the Math class
+            Math math = new Math();
+
+            //calls the Add method with two numbers passed in
+            math.Add(15, 20);
+
+            //calls the Add methods and specifies the parameters by name
+            math.Add(b: 30, a: 25);
+
+            Console.ReadLine();
+        }
+    }
+}
