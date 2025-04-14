bbdcdaa26acf59098117f283dcbb6d1e06a63c50
KaityBadge <kaitybadge@yahoo.com>
2025-04-14 15:04:21 -0700

    try/catch block case insensitive

---
+++ ParsingEnums/ParsingEnums/Days.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    /// This is an enum that represents the constants for the days of a week.
+    public enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ParsingEnums
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user to enter the current day of the week
+            Console.WriteLine("Please enter the current day of the week (e.g. Monday):");
+
+            //try/catch block will catch any exceptions that may occur
+            try
+            {
+                //takes in user input
+                 string userInput = Console.ReadLine();
+
+                //userInput is parsed from string to the enum type DaysOfWeek and stored as a variable called 'day'
+                //true is used to ignore case sensitivity so user can enter 'monday' or 'Monday'
+                DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+                Console.WriteLine("You entered: " + day);
+            }
+            //catch block will 'throw and exception' which informs the user if the input is invalid
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter and actual day of the week.");
+            }
+                   
+            Console.ReadLine();
+        }
+    }
+}
