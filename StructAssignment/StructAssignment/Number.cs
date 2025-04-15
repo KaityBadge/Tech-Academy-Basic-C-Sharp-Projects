87f1ddb5c10640195b56e5a2020221c97b4777e0
KaityBadge <kaitybadge@yahoo.com>
2025-04-15 09:38:08 -0700

    practice using structs

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+   public struct Number
+    {
+        //gives Number a property called Amount of the decimal type
+        public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StructAssignment
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates an object of the data type Number (struct)
+            //and assigns it to the variable number
+            Number number = new Number();
+
+            //sets the Amount property to 100.00
+            number.Amount = 100.00m;
+
+            //displays the value of the Amount property in the console
+            Console.WriteLine("The amount is: " + number.Amount);
+            
+            Console.ReadLine();
+        }
+    }
+}
