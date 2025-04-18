e94edd7c1830ec2a051d57f656bbf32e1becd190
KaityBadge <kaitybadge@yahoo.com>
2025-04-18 16:52:20 -0700

    logs user input to filepath

---
+++ InputLogAssignment/InputLogAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Net.WebRequestMethods;
+
+
+namespace InputLogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user for a number
+            Console.WriteLine("Please enter a number:");
+            //read the number from the console
+            string number = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\kaity\Logs\practice.txt", true))
+            {
+                //write the number to the file
+                file.WriteLine(number);
+            }
+
+
+        }
+    }
+}
