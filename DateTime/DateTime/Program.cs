e2a56098e17e588dba0e920443d288d776685d25
KaityBadge <kaitybadge@yahoo.com>
2025-04-18 17:31:00 -0700

    applies user input to DateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //gets the current date and time
+            DateTime currentDateTime = DateTime.Now;
+
+            //prints the current date and time in the console
+            Console.WriteLine("The current Date and Time is: " + currentDateTime);
+
+            //prompts user for a number
+            Console.WriteLine("Enter a number: ");
+            //stores input in variable "number"
+            string number = Console.ReadLine();
+
+            //gets the time it will be in "number" hours from the current date and time
+            DateTime futureDateTime = currentDateTime.AddHours(Convert.ToDouble(number));
+
+            //prints the future date and time in the console
+            Console.WriteLine("In " + number + " hours it will be: " + futureDateTime);
+
+            Console.ReadLine();
+        }
+    }
+}
