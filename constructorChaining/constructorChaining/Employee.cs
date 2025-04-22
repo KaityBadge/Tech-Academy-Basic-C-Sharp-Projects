d1112572d7668cc71361b1da854537e49f020e80
KaityBadge <kaitybadge@yahoo.com>
2025-04-22 11:53:34 -0700

    created a class that uses constructor chaining

---
+++ constructorChaining/constructorChaining/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorChaining
+{
+    // This Employee class demonstrates constructor chaining
+    public class Employee
+    {
+        public string Name;
+        public string Department;
+
+        // First constructor
+        // takes no parameters ( )
+        // uses :this() to call the constructor that takes one parameter
+        public Employee() : this ("Employee 1")
+        {
+            Console.WriteLine("Default constructor is called.");
+
+        }
+
+
+        // Second constructor
+        // takes one parameter
+        // uses :this() to call the constructor that takes two parameters
+        public Employee(string name) : this(name, "unknown")
+        {
+            
+        }
+
+        // Third constructor
+        // takes two parameters 
+        // this constructor passes the parameters to the class properties
+        public Employee(string name, string department)
+        {
+            this.Name = name;
+            this.Department = department;
+            Console.WriteLine("Constructor with two parameters is called.");
+        }
+    }
+}
---
+++ constructorChaining/constructorChaining/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace ConstructorChaining
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program that demonstrates constructor chaining in the Employee class.
+            // It creates an instance of the Employee class using different constructors.
+
+            //creates a constant for the business name
+            const string buisinessName = "Tech Solutions";
+            // prompts the user for their name and welcomes them to the employee site.
+            Console.WriteLine("Welcome to the employee site for {0}! What is your name?" , buisinessName);
+            // reads the user's input and stores it in the empName variable
+            var empName = Console.ReadLine();
+            Console.WriteLine("\nHello, {0}! Feel free to look around the {1} site." , empName, buisinessName);
+
+            Console.ReadLine();
+        }
+    }
+}
