70d9f014edacc9dbf2cbc80880707ab074f8f084
KaityBadge <kaitybadge@yahoo.com>
2025-05-15 12:48:08 -0700

    date time time stamp

---
+++ MyFirstWebApp/MyFirstWebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstWebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet() //event handler method OnGet
+        {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
